f947c517625400df7fdc5c455bad2f931a00982f
anakuma3 <haruna.may23@gmail.com>
2024-03-11 17:40:22 +0900

    FizzBuzzプログラムを作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+number = (1..20).to_a
+number.each do |x|
+  case 
+  when x % 3 ==0 && x % 5 ==0
+    puts "FizzBuzz"
+  when x % 3 ==0
+    puts "Fizz"
+  when x % 5 ==0
+    puts "Buzz"
+    else
+    puts x
+  end
+end
+
